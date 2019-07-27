5137faeedd67f444adea940b18cdd23d1289d732
tobibako45 <tobibako45@gmail.com>
2019-07-27 17:44:48 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <Calc />
+    <Calc v-bind:title="message" v-on:result-event="appAction" />
+    <hr />
+    <div>
+      <table v-html="log"></table>
+    </div>
   </div>
 </template>
 
@@ -11,17 +15,68 @@ export default {
   name: "app",
   components: {
     Calc
+  },
+  data: function() {
+    return {
+      message: "CALC",
+      result: []
+    };
+  },
+  computed: {
+    log: function() {
+      var table =
+        '<tr><th class="head">Expression</th><th class="head">Value</th></tr>';
+      for (var i in this.result) {
+        table +=
+          "<tr><td>" +
+          this.result[i][0] +
+          "</td><th>" +
+          this.result[i][1] +
+          "</th></tr>";
+      }
+      return table;
+    }
+  },
+  created: function() {
+    var items = localStorage.getItem("log");
+    var logs = JSON.parse(items);
+    if (logs != null) {
+      this.result = logs;
+    }
+  },
+  methods: {
+    appAction: function(exp, res) {
+      this.result.unshift([exp, res]);
+      if (this.result.length > 10) {
+        this.result.pop();
+      }
+      var log = JSON.stringify(this.result);
+      localStorage.setItem("log", log);
+    }
   }
 };
 </script>
+
 
 <style>
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 5px;
+}
+tr td {
+  padding: 5px;
+  border: 1px solid gray;
+}
+tr th {
+  padding: 5px;
+  border: 1px solid gray;
+}
+tr th.head {
+  background-color: black;
+  color: white;
 }
 </style>
--- src/components/Calc.vue
+++ src/components/Calc.vue
@@ -5,9 +5,11 @@
     <hr />
     <div>
       <div>
+        <!-- v-model="fomula"として、foumlaという変数にに値が同期される -->
         <textarea v-model="fomula" cols="40" rows="5"></textarea>
       </div>
       <div>
+        <!-- ボタンでは、v-on:click="doAction"として、クリックしたらdoActionが実行される -->
         <button v-on:click="doAction">CALC</button>
       </div>
     </div>
@@ -18,35 +20,55 @@
 export default {
   name: "Calc",
   props: {
+    // titleは後で操作することまずないので、属性としてpropsで設定してある
     title: String
   },
+  // messageとfoumlaは常に変更されるものであるためdataに用意してある
   data: function() {
     return {
       message: "式を入力してください！ Enter expression",
-      fomula: 0
+      fomula: "0"
     };
   },
   methods: {
+    // ボタンでクリックした際に実行されるdoActionが用意されている。
+    // テキストエリアで記述された値(v-modelでfomulaに値が保管される)をもとに、
+    // 入力された式を実行し結果を取り出しています。
     doAction: function() {
+      // fomulaのテキストを、trim()で前後の空白を取り除いて、split()で、改行コードで分割し配列にする
       var arr = this.fomula.trim().split("¥n");
+      // pop()で配列の最後の項目を変数lastに取り出しておく(配列は後から消える)
       var last = arr.pop();
       var fn = "";
+
+      // 配列の各要素について繰り返し処理する
       for (var n in arr) {
+        // 繰り返しでは、取り出した項目のテキストが空でないなら、「var テキスト;」というテキストを作って変数fnに追加する
         if (arr[n].trim() != "") {
           fn += "var " + arr[n] + ";";
         }
       }
-
+      // 繰り返しが終わったら「return 最後の項目;」とテキストを追加して完成。
       fn += "return " + last + ";";
+
+      // 完成したテキストの前後に「function(){」「} f();」と付け足す。
+      // これで、取り出した処理のテキストをfという関数にまとめて実行するスクリプトができあがる
       var exp = "function f(){" + fn + "} f();";
+      // eval()を遣ってスクリプトのテキストを実行する。eval() は文字列として表された JavaScript コードを式として評価する関数です。
       var ans = eval(exp);
+      // 実行結果をメっセージにまとめておく
       this.message = "答え answer: " + ans;
-      var re = arr.json(":").trim();
+      // join(';')で、配列にセミコロン(;)記号でつなげて１つのテキストにする。trim()で前後の空白を削除
+      var re = arr.join(";").trim();
+      // 配列が空でなければセミコロンを付け、最後の項目に付け加える
       if (re != "") {
-        re += last;
-        this.$emit("result-event", re, ans);
+        re += ";";
       }
+      re += last;
+      // 作成したテキストを引数に指定し、$emitでresult-eventイベントを呼び出す
+      this.$emit("result-event", re, ans);
     }
   }
 };
 </script>
+
